1b8b5c8b14205ab163fc4e85ca4cf764f2a4104d
zhulu <zhulu555@sina.com>
2022-08-13 09:21:05 +0800

    a

---
+++ app/login/interface/clockininterface.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"context"
+
+	pb "api/login/interface/v1"
+)
+
+type ClockinInterfaceService struct {
+	pb.UnimplementedClockinInterfaceServer
+}
+
+func NewClockinInterfaceService() *ClockinInterfaceService {
+	return &ClockinInterfaceService{}
+}
+
+func (s *ClockinInterfaceService) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterReply, error) {
+	return &pb.RegisterReply{}, nil
+}
+func (s *ClockinInterfaceService) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginReply, error) {
+	return &pb.LoginReply{}, nil
+}
+func (s *ClockinInterfaceService) ClockinOnWork(ctx context.Context, req *pb.ClockinOnWorkRequest) (*pb.ClockinOnWorkReply, error) {
+	return &pb.ClockinOnWorkReply{}, nil
+}
+func (s *ClockinInterfaceService) ClockinOffWork(ctx context.Context, req *pb.ClockinOffWorkRequest) (*pb.ClockinOffWorkReply, error) {
+	return &pb.ClockinOffWorkReply{}, nil
+}
---
+++ app/user/userservice.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"context"
+
+	pb "api/user/v1"
+)
+
+type UserServiceService struct {
+	pb.UnimplementedUserServiceServer
+}
+
+func NewUserServiceService() *UserServiceService {
+	return &UserServiceService{}
+}
+
+func (s *UserServiceService) GetUserById(ctx context.Context, req *pb.GetUserByIdRequest) (*pb.GetUserByIdReply, error) {
+	return &pb.GetUserByIdReply{}, nil
+}
+func (s *UserServiceService) GetUserByName(ctx context.Context, req *pb.GetUserByNameRequest) (*pb.GetUserByNameReply, error) {
+	return &pb.GetUserByNameReply{}, nil
+}
+func (s *UserServiceService) SearchUserByName(ctx context.Context, req *pb.SearchUserByNameRequest) (*pb.SearchUserByNameReply, error) {
+	return &pb.SearchUserByNameReply{}, nil
+}
+func (s *UserServiceService) SaveUser(ctx context.Context, req *pb.SaveUserRequest) (*pb.SaveUserReply, error) {
+	return &pb.SaveUserReply{}, nil
+}
+func (s *UserServiceService) RemoveUser(ctx context.Context, req *pb.RemoveUserRequest) (*pb.RemoveUserReply, error) {
+	return &pb.RemoveUserReply{}, nil
+}
